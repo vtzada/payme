5dd5c825ea12d21c5339440a36b8ea90bce12cc8
Vitor <vitortheof@gmail.com>
2026-08-27 12:28:15 -0300

    first commit

---
+++ src/main/java/br/com/vitortheof/payme/PaymeApplication.java
@@ -0,0 +1,13 @@
+package br.com.vitortheof.payme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/vitortheof/payme/PaymeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vitortheof.payme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
