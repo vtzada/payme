a207b72dd17fd51cb81a20afd14b074a84edc97b
Vitor <vitortheof@gmail.com>
2026-09-18 18:32:35 -0300

    feat: add validation customer

--- src/main/java/br/com/vitortheof/payme/user/domain/Customer.java
+++ src/main/java/br/com/vitortheof/payme/user/domain/Customer.java
@@ -2,28 +2,34 @@ package br.com.vitortheof.payme.user.domain;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Entity
-@Table(name = "tb_users")
+@Table(name = "customers")
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class User {
+public class Customer {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
+    @Column(nullable = false)
     private String name;
 
+    @Column(nullable = false)
     private String email;
 
+    @Column(nullable = false)
     private String password;
 
-    private LocalDateTime createdAt = LocalDateTime.now();
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
 }
