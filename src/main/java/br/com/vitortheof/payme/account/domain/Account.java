04271c8b680f0fea6f7041e7bc658d2573f72ae3
Vitor <vitortheof@gmail.com>
2026-09-20 18:26:05 -0300

    fix: validation em account

--- src/main/java/br/com/vitortheof/payme/account/domain/Account.java
+++ src/main/java/br/com/vitortheof/payme/account/domain/Account.java
@@ -2,22 +2,46 @@ package br.com.vitortheof.payme.account.domain;
 
 import br.com.vitortheof.payme.account.domain.enums.AccountType;
 import br.com.vitortheof.payme.account.domain.enums.SyncType;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.UUID;
 
+@Entity
+@Table(name = "account")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Account {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
-    private UUID userId;
+    @Column(name = "costumer_id", nullable = false)
+    private UUID customerId;
 
+    @Column(nullable = false)
     private String name;
 
+    @Column(nullable = false)
     private BigDecimal balance;
 
+    @Enumerated(EnumType.STRING)
+    @Column(name = "account_type", nullable = false)
     private AccountType accountType;
-    
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "sync_type", nullable = false)
     private SyncType syncType;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
 
 }
